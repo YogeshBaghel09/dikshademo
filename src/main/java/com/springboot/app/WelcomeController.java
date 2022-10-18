c02eb599e493b212223c8e8c9a89e70fc356ceb3
YogeshBaghel09 <115460005+YogeshBaghel09@users.noreply.github.com>
2022-10-18 11:21:53 +0530

    Update WelcomeController.java

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
  @GetMapping("/welcome")
 	public String welcome() {
-	 return "Welcome to Diksha Demo hosted by Hitachi India changes done in demo Harshit";
+	 return "Welcome to Diksha Demo hosted by Hitachi India";
  }
 }
